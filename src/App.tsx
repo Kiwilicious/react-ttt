18bab729c2845c63e5ece53201f35d7652786339
Kiwilicious <jhwang137@gmail.com>
2024-08-14 17:19:57 -0700

    feat: add title and reamdme

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import Board from './components/Board'
 
 function App() {
   return (
-    <div className="flex flex-col container mx-auto p-4 items-center">
+    <div className="flex flex-col container mx-auto p-8 items-center">
+      <h1 className="text-2xl mb-2">React TicTacToe</h1>
       <Board />
     </div>
   )
