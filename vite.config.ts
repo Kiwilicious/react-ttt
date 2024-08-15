422b2d9cc45bc4ed7b3b338b3209d9e5074f4acf
Kiwilicious <jhwang137@gmail.com>
2024-08-14 18:29:17 -0700

    chore: remove unused asset ref and fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // change for gh-pages asset lookup path
-    assetsDir: 'react-ttt/assets',
-  },
+  // https://vitejs.dev/guide/static-deploy#github-pages
+  base: '/react-ttt/',
 })
